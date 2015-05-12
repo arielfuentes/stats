14db5f0c2c6338aba4d470fb96f3290a6cb413fd
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2015-05-12 12:53:15 -0300

    Update TRX.R

--- TRX.R
+++ TRX.R
@@ -6,7 +6,10 @@ par$ICF <- as.numeric(par$ICF)
 par <- par[!par$Servicio == "B07C",]
 parTN <- par[!(par$Mes == 1 | par$Mes == 2),]
 parTN <- parTN[(par$Anho == 2014 | par$Anho == 2015 ),]
-pairs(~TRX+Prom.ICR+ Capacidad.de.Transporte+KM+PLAZASxKM+Plaza.Teorica+ICT+Plaza.Ofrecida+ICF, 
-      data = parTN, main = "Matriz de dispersión")
-pdf("rplotx.pdf", width = 2200, height = 2000)
-dev.off()
+for (i in parTN$Servicio) {
+  parTNSS <- subset(parTN, Servicio == i & (Tipo.Dia == "LABORAL"))
+  jpeg(filename = paste(i, "TN", "DLN2", sep="", ".jpg"), width = 2200, height = 2000)
+  pairs(~TRX+Prom.ICR+ Capacidad.de.Transporte+KM+PLAZASxKM+Plaza.Teorica+ICT+Plaza.Ofrecida+ICF, 
+        data = parTNSS, main = i)
+  dev.off()
+  }
