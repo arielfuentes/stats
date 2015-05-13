0e098155eae6691d1d411c5b30ee5d5152f454a5
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2015-05-13 11:53:09 -0300

    Update and rename TRX.R to ParamOP_PMA.R

---
+++ ParamOP_PMA.R
@@ -0,0 +1,26 @@
+#######Directory
+setwd("~/ariel")
+######Cleaning
+par <- read.csv(file = "Indicador4.csv",sep=";",header = T)
+levels(par$ICT)[2] <- NA
+par$ICT  <- as.numeric(par$ICT)
+levels(par$ICF)[2] <- NA
+par$ICF <- as.numeric(par$ICF)
+par <- par[!(par$Servicio == "B07C" | par$Servicio == "B09C" | par$Servicio == "B90" | par$Servicio == "B91" | par$Servicio == "SINSER" | par$Servicio == "" | par$Servicio == "B92" | par$Servicio == "B94"),]
+par$Servicio <- droplevels(par$Servicio)
+##############PMA
+parPMA <- par[!(par$Servicio == "B51C" | par$Servicio == "B60E" | par$Servicio == "B61C" | par$Servicio == "B71"), ]
+#rm(par)
+parTNPMA <- parPMA[!(parPMA$Mes == 1 | parPMA$Mes == 2),]
+parTNPMA <- parTNPMA[(parTNPMA$Anho == 2014 | parTNPMA$Anho == 2015 ),]
+#########Images
+for (i in parTNPMA$Servicio) {
+  #for (j in parTN$Periodo) {
+    parTNSS <- subset(parTNPMA, Servicio == i & (Periodo == "PMA"))
+    jpeg(filename = paste(i, "TN", "PMA", sep="", ".jpg"), width = 2200, 
+         height = 2000)
+    pairs(~TRX+Prom.ICR+ Capacidad.de.Transporte+KM+PLAZASxKM+Plaza.Teorica+ICT+
+            Plaza.Ofrecida+ICF, data = parTNSS, main = paste(i, "PMA"))
+    dev.off()
+  #}
+}
